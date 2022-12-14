eef1111dc8790eb2ea726f9f555bc0a54009beeb
lhseunge <lhseunge@gmail.com>
2022-12-15 00:25:18 +0900

    편지

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -8,16 +8,10 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().solution(15000)));
+        System.out.println(new Solution().solution("i love you~"));
     }
 
-    public int[] solution(int money) {
-
-        int coffePrice = 5500;
-        int cupOfCoffee = money / coffePrice;
-
-        int exchange = money - cupOfCoffee * coffePrice;
-
-        return new int[]{cupOfCoffee, exchange};
+    public int solution(String message) {
+        return message.length() * 2;
     }
 }
