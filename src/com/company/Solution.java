89cb8f982647c7f451c4689e4eadc9eac6bebb9c
lhseunge <lhseunge@gmail.com>
2022-12-15 00:04:49 +0900

    배열 자르기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -3,17 +3,23 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(7, 10));
+        System.out.println(Arrays.toString(new Solution().solution(new int[]{1, 3, 5}, 1, 2)));
     }
 
-    public int solution(int slice, int n) {
+    public int[] solution(int[] numbers, int num1, int num2) {
 
-        double pizza = (double) n / slice;
+        List<Integer> answerList = new ArrayList<>();
 
-        return (int) Math.ceil(pizza);
+        for (int i = num1; i <= num2; i ++) {
+            answerList.add(numbers[i]);
+        }
+
+        int[] answer = answerList.stream().mapToInt(x->x).toArray();
+        return answer;
     }
 }
