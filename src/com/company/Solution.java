37cf5c133165cea2b541188cc0f8e476c456cf60
lhseunge <lhseunge@gmail.com>
2022-12-22 00:13:19 +0900

    최댓값 만들기(1)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -2,25 +2,21 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution("hello", 3));
+        System.out.println(new Solution().solution(new int[]{1, 2, 3, 4, 5}));
     }
 
-    public String solution(String my_string, int n) {
+    public int solution(int[] numbers) {
 
-        char[] temp = my_string.toCharArray();
+        Integer[] result = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        Arrays.sort(result, Collections.reverseOrder());
 
-        StringBuilder answerBuilder = new StringBuilder();
-
-        for(char c : temp) {
-            answerBuilder.append(String.valueOf(c).repeat(n));
-        }
-
-        return answerBuilder.toString();
+        return result[0] * result[1];
     }
 }
