6091be0e8880bf946239cafe484a706b1920b9fb
lhseunge <lhseunge@gmail.com>
2022-12-24 23:27:45 +0900

    삼각형의 완성조건 (1)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -9,14 +9,17 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(new Solution().solution(new int[]{1, 2, 3}));
     }
 
-    public int solution(int[] numbers) {
+    public int solution(int[] sides) {
 
-        Integer[] result = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-        Arrays.sort(result, Collections.reverseOrder());
+        Arrays.sort(sides);
 
-        return result[0] * result[1];
+        if(sides[0] + sides[1] > sides[2]) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
