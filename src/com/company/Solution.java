a1c8267867783d105f2a25716d2d0cadcd4e8223
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-01-06 17:41:17 +0900

    자릿수 더하기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -9,24 +9,20 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2,1,20));
+        System.out.println(new Solution().solution(930211));
     }
 
-    /**
-     * a : 콜라를 받으려면 마트에 줘야하는 병 수
-     * b : 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수
-     * c : 상빈이가 가지고 있는 빈 병의 수
-     */
-    public int solution(int a, int b, int n) {
+    public int solution(int n) {
 
-        List<Integer> calc = new ArrayList<>() ;
+        int answer = 0;
+        String nToString = n+"";
 
-        while (n >= a) {
-            calc.add(n);
-            n = n - a + b;
+        char[] nToCharArray = nToString.toCharArray();
+
+        for (char c : nToCharArray) {
+            answer += Integer.parseInt(String.valueOf(c));
         }
 
-        return calc.size() * b;
-
+        return answer;
     }
 }
