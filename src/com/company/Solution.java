e59ce733d11fee7181a4a65dcff42d707ea23118
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-01-10 11:07:10 +0900

    개미 군단

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -9,20 +9,46 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(20));
+        System.out.println(new Solution().solution(999));
     }
 
-    public int solution(int n) {
+    public int solution(int hp) {
 
         int answer = 0;
+        int atkCnt = 0;
 
-        for (int i = 1; i <= n; i++) {
-            if(n % i == 0) {
-
+        while(hp != 0) {
+            if (hp >= 5) {
+                atkCnt = atkGeneral(hp);
+                hp -= atkCnt * 5;
+                answer += atkCnt;
+                continue;
+            }
+            if (hp >= 3) {
+                atkCnt = atkSoldier(hp);
+                hp -= atkCnt * 3;
+                answer += atkCnt;
+                continue;
+            }
+            if (hp >= 1) {
+                hp = atkWorker(hp);
                 answer++;
+                continue;
             }
         }
 
         return answer;
+    }
+
+    public int atkGeneral(int hp) {
+        return hp / 5;
+    }
+
+    public int atkSoldier(int hp) {
+        return hp / 3;
+    }
+
+    public int atkWorker(int hp) {
+        return hp - 1;
     }
 }
