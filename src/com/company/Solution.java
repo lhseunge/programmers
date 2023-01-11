8e7e521654b495d4153565cb83bb6127ac07a809
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-01-11 16:17:51 +0900

    최빈값 구하기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -9,15 +9,44 @@ import java.util.stream.Collectors;
 import java.util.Scanner;
 
 public class Solution {
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        System.out.println(new Solution().solution(new int[]{1, 1}));
+    }
 
-        StringBuilder starTree = new StringBuilder("*");
+    public int solution(int[] array) {
 
-        for(int i = 0; i < n; i++) {
-            System.out.println(starTree);
-            starTree.append("*");
+        if (array.length == 1) {
+            return array[0];
         }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] distinctArray = Arrays.stream(array).distinct().toArray();
+
+        for (int i : distinctArray) {
+            map.put(i, (int) Arrays.stream(array).filter(x-> x == i).count());
+
+        }
+
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+
+        entryList.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        if(entryList.size() == 1) {
+            return entryList.get(0).getKey();
+        }
+
+        if(entryList.get(0).getValue() == entryList.get(1).getValue()) {
+            return -1;
+        }
+
+        return entryList.get(0).getKey();
+
     }
 }
