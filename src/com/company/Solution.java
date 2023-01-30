9d017fd94beb4b1773de7425b8dedb632d6076b2
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-01-30 18:57:23 +0900

    진료순서 정하기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -11,14 +11,29 @@ import java.util.Scanner;
 public class Solution {
     
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(29423));
+        System.out.println(Arrays.toString(new Solution().solution(new int[]{1,100,2,88,99})));
     }
 
-    public int solution(int order) {
+    public int[] solution(int[] emergency) {
 
-        return Arrays.stream(String.valueOf(order).split(""))
-                .filter(x -> x.equals("3") || x.equals("6") || x.equals("9"))
-                .collect(Collectors.joining()).length();
 
+        int[] emergencyRank = new int[emergency.length];
+        int[] sortedEmergency = Arrays.stream(emergency).boxed().collect(Collectors.toList()).stream().sorted(Collections.reverseOrder()).mapToInt(x->x).toArray();
+
+        int rank = 1;
+
+        for (int i = 0; i < emergency.length; i++) {
+            for (int  j = 0; j < emergency.length; j++) {
+
+                if (sortedEmergency[i] == emergency[j]) {
+                    emergencyRank[j] = rank;
+                    rank++;
+                    break;
+                }
+            }
+
+        }
+
+        return emergencyRank;
     }
 }
