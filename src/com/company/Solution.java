e225247497ef9fbea1b181d8ca0716c58246a1f7
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-02-15 16:51:44 +0900

    중복된 문자 제거

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,30 +7,11 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(15));
+        System.out.println(new Solution().solution("people"));
     }
-    
-    public int solution(int n) {
 
-        Set<Integer> answer = new HashSet<>();
+    public String solution(String my_string) {
+       return Arrays.stream(my_string.split("")).distinct().collect(Collectors.joining());
 
-        Set<Integer> numbers = new HashSet<>(); // 약수
-
-        for (int i = 1; i <= n ; i++) {
-
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    numbers.add(j);
-                }
-            }
-
-            if (numbers.size() >= 3) {
-                answer.add(i);
-            }
-            numbers.clear();
-
-        }
-
-        return answer.size();
     }
 }
