94748563cf64fd8f7007ed1d5708198d08856040
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-02-16 17:59:50 +0900

    한 번만 등장한 문자

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,28 +7,20 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(1,13, 1));
+        System.out.println(new Solution().solution("helloo"));
     }
 
-    /**
-     * i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
-     * @param i 시작 숫자
-     * @param j 끝 숫자
-     * @param k 타겟
-     * @return
-     */
-    public int solution(int i, int j, int k) {
+    public String solution(String s) {
 
-        int answer = 0;
+        String[] sArray = Arrays.stream(s.split("")).toArray(String[]::new);
 
-        for (int idx = i; idx <= j; idx++) {
-            for (String s : String.valueOf(idx).split("")) {
-                if (s.equals(String.valueOf(k))) {
-                    answer++;
-                };
+        for (String word : sArray) {
+            if (Arrays.stream(sArray).filter(x -> x.equals(word)).count() > 1L) {
+                s = s.replace(word, "");
             }
         }
 
-        return answer;
+        return Arrays.stream(s.split("")).sorted().collect(Collectors.joining());
+
     }
 }
