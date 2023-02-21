528bbaf45d0064ef3fedd034719cf7cf8d5b3a7e
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-02-21 16:43:27 +0900

    숨어있는 숫자의 덧셈 (2)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,27 +7,13 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution("onefourzerosixseven"));
+        System.out.println(new Solution().solution("aAb1B2cC34oOp"));
     }
 
-    public long solution(String numbers) {
+    public int solution(String my_string) {
 
-        List<String> nums = new ArrayList<>();
-        nums.add("zero");
-        nums.add("one");
-        nums.add("two");
-        nums.add("three");
-        nums.add("four");
-        nums.add("five");
-        nums.add("six");
-        nums.add("seven");
-        nums.add("eight");
-        nums.add("nine");
+        my_string = my_string.replaceAll("[a-zA-Z]", " ").trim();
 
-        for (int i = 0; i < nums.size(); i++) {
-            numbers = numbers.replace(nums.get(i), String.valueOf(i));
-        }
-
-        return Long.parseLong(numbers);
+        return Arrays.stream(my_string.split(" ")).filter(x-> !x.isEmpty()).mapToInt(Integer::parseInt).sum();
     }
 }
