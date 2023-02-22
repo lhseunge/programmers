c71dcb871159e833ab05c52a030d93411b8a6e18
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-02-22 16:51:17 +0900

    로그인 성공?

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,13 +7,19 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2, 5));
+        System.out.println(new Solution().solution(new String[]{"meosseugi", "1234"}, new String[][]{{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "12345"}}));
     }
 
-    public int solution(int M, int N) {
+    public String solution(String[] id_pw, String[][] db) {
 
-        if (M == 1 && N == 1) return 0;
+        for (String[] strings : db) {
+            if (strings[0].equals(id_pw[0])) {
+                if (strings[1].equals(id_pw[1])) return "login";
+                else return "wrong pw";
+            }
 
-        return (M - 1) + M * (N - 1);
+        }
+
+        return "fail";
     }
 }
