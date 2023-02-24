fbaa57817cb2c1b4201501f3dd2594e54d81fd59
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-02-24 18:07:33 +0900

    OX퀴즈

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,19 +7,29 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new String[]{"meosseugi", "1234"}, new String[][]{{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "12345"}}));
+        System.out.println(Arrays.toString(new Solution().solution(new String[]{"2190 - -6344 = -8534", " - 5 + 66 = 61", "5 - 15 = 63", " - 3 - 0 = -3"})));
     }
 
-    public String solution(String[] id_pw, String[][] db) {
+    public String[] solution(String[] quiz) {
 
-        for (String[] strings : db) {
-            if (strings[0].equals(id_pw[0])) {
-                if (strings[1].equals(id_pw[1])) return "login";
-                else return "wrong pw";
+        List<String> result = new ArrayList<>();
+
+        for (String s : quiz) {
+            int[] temp = Arrays.stream(s.trim()
+                                        .replace("- ", "-")
+                                        .replace("--", "+ ")
+                                        .replace("+ ", "")
+                                        .replace("= ","")
+                                        .split(" ")
+                    ).mapToInt(Integer::parseInt).toArray();
+
+            if (temp[0] + temp[1] == temp[2]) {
+                result.add("O");
+            } else {
+                result.add("X");
             }
-
         }
 
-        return "fail";
+        return result.toArray(String[]::new);
     }
 }
