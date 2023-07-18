2655845680444fde6b68bcef0d0cd492445c64b3
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-07-18 17:06:47 +0900

    특이한 정렬

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,33 +1,32 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution(40));
+        int[] numlist = {
+                10000, 20, 36, 47, 40, 6, 10, 7000, 50, 10
+        };
+
+        System.out.println(Arrays.toString(new Solution().solution(numlist, 30)));
     }
 
-    public int solution(int n) {
-        int answer = 0;
+    public int[] solution(int[] numlist, int n) {
 
-        for (int i = 1; i <= n; i++) {
+        int[] answer = {};
 
-            answer++;
+        List<Integer> numList = Arrays.stream(numlist).boxed().sorted().collect(Collectors.toList());
 
-            answer = numCheck(answer);
+        numList.sort((o1, o2) -> Math.abs(o2 - n) - Math.abs(o1 - n));
 
-        }
+        Collections.reverse(numList);
 
-        return answer;
-    }
-
-    private int numCheck(int answer) {
-
-        if (Arrays.asList(String.valueOf(answer).split("")).contains("3") || answer % 3 == 0) {
-            return numCheck(answer + 1);
-        }
+        answer = numList.stream().mapToInt(x -> x).toArray();
 
         return answer;
     }
