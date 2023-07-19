3ca70d023779f29bdbf97e530cf9b589c04df9e1
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-07-19 12:54:27 +0900

    다항식 더하기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,28 +1,49 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution("abca", "aabc"));
+        System.out.println(new Solution().solution("x + x + 7"));
     }
 
-    public int solution(String A, String B) {
+    public String solution(String polynomial) {
+        String answer = "";
 
-        if (A.equals(B)) {
-            return 0;
+        List<String> splitPolynomial = Arrays.stream(polynomial.split(" [+] ")).collect(Collectors.toList());
+
+
+        int vars = splitPolynomial.stream()
+                .filter(term -> term.contains("x") && !term.equals("x"))
+                .map(term -> term.replace("x", ""))
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        int onlyVars = (int) splitPolynomial.stream()
+                .filter(term -> term.equals("x"))
+                .count();
+
+
+        int constant = splitPolynomial.stream()
+                .filter(term -> !term.contains("x"))
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        String varSum = vars + onlyVars == 1 ? "x" : (vars + onlyVars)+"x";
+
+        if (vars + onlyVars > 0 && constant > 0) {
+            answer = varSum + " + " + constant;
+        } else if (vars + onlyVars > 0) {
+            answer = varSum;
+        } else if (constant > 0) {
+            answer = "" + constant;
         }
 
-        for (int i = 1; i <= A.length(); i++) {
 
-            A = A.split("")[A.length() - 1].concat(A).substring(0, A.length());
-
-            if (A.equals(B)) {
-                return i;
-            }
-        }
-
-        return -1;
-
+        return answer;
     }
 }
