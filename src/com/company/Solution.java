00709314c23d528bdaed3f8eae669de4f29fb541
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-07-24 15:48:00 +0900

    겹치는 선분의 길이

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,89 +1,56 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        int[][] board = {
-                // round 1
-/*
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0}
-
-                // round 2
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 1, 1, 0},
-                {0, 0, 0, 0, 0}
-
-                // round 3
-                {1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1}
-
-                // round 4
-                {0, 0, 0, 0, 1},
-                {0, 1, 0, 1, 0},
-                {0, 1, 0, 0, 0},
-                {0, 0, 1, 0, 0},
-                {0, 1, 0, 0, 0}
-*/
-
-                // round 5
-                {1, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 1}
+        int[][] lines = {
+                {0, 5}, {3, 9}, {1, 10}
         };
 
-        System.out.println(new Solution().solution(board));
-    }
-
-    public int solution(int[][] board) {
-
-        int answer;
-        int boardWidth = board.length;
 
         /*
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 0}
+        0, 1
+              2, 3, 4, 5
+                 3, 4, 5, 6, 7, 8, 9
         */
 
-        // 이중 반복문을 사용하여 주변 요소들에 대해 반복적으로 처리
-        for (int i = 0; i < boardWidth; i++) {
-            for (int j = 0; j < boardWidth; j++) {
-                // 현재 요소가 1인 경우에만 처리
-                if (board[i][j] == 1) {
-                    // 인덱스 확인하여 경계를 벗어나지 않도록 처리
-                    for (int row = Math.max(0, i - 1); row <= Math.min(boardWidth - 1, i + 1); row++) {
-                        for (int col = Math.max(0, j - 1); col <= Math.min(boardWidth - 1, j + 1); col++) {
-                            // 현재 요소가 1이면 1로, 그렇지 않으면 -1로 설정
-                            board[row][col] = board[row][col] == 1 ? 1 : -1;
-                        }
-                    }
-                }
+        /*
+        -1, 0, 1
+               1, 2, 3
+                     3, 4, 5, 6, 7, 8, 9
+        */
+
+        /*
+        0, 1, 2, 3, 4, 5
+           1, 2, 3, 4, 5, 6, 7, 8, 9 ,10
+                 3, 4, 5, 6, 7, 8, 9
+        */
+
+        System.out.println(new Solution().solution(lines));
+    }
+
+    public int solution(int[][] lines) {
+        
+        int answer = 0;
+
+        int[] rail = new int[200];
+        for (int[] line : lines) {
+            for (int j = (line[0] + 100); j < (line[1] + 100); j++) {
+                rail[j]++;
             }
         }
 
-        answer = (int) Arrays.stream(board)
-                .flatMapToInt(Arrays::stream)
-                .filter(x -> x == 0)
-                .count();
-
+        for (int value : rail) {
+            if (value > 1) answer++;
+        }
+        
         return answer;
+
     }
 
 }
