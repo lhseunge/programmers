35da044f3e9f1d65f4ac68c2ab71fd5588d13f29
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-12-27 00:45:37 +0900

    평행
    참조한 해설블로그와 로직은 동일함
    
    평행을 확인하기 위해 기울기를 사용한다는 개념을 다른 문제에도 적용할 수 있도록.
    
    ---
    
    [[프로그래머스][JAVA] 평행](https://velog.io/@shin75492/프로그래머스JAVA-평행)
    
    [기울기 복습 (개념 이해하기) | 일차방정식과 그래프 | Khan Academy](https://ko.khanacademy.org/math/algebra/x2f8bb11595b61c86:linear-equations-graphs/x2f8bb11595b61c86:slope/a/slope-review)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,17 +1,64 @@
-package com.company;
-
-import java.util.Scanner;
-
 public class Solution {
 
     public static void main(String[] args) {
-        String msg = "Spring is beginning";
-        int val1 = 3;
-        String val2 = "3";
 
-        System.out.println(msg);
-        System.out.println(val1 + 10);
-        System.out.println(val2 + "10");
+        int[][] lines = {
+                {1, 4},
+                {9, 2},
+                {3, 8},
+                {11, 6}
+        };
+
+        int[][] lines2 = {
+                {3, 5},
+                {4, 1},
+                {2, 4},
+                {5, 10}
+        };
+
+        System.out.println(new Solution().solution(lines));
+    }
+
+    public int solution(int[][] lines) {
+        int answer = 0;
+
+        int x1 = lines[0][0];
+        int y1 = lines[0][1];
+        int x2 = lines[1][0];
+        int y2 = lines[1][1];
+        int x3 = lines[2][0];
+        int y3 = lines[2][1];
+        int x4 = lines[3][0];
+        int y4 = lines[3][1];
+
+        // 선분 별 각각의 기울기 계산
+
+        double slopeCase1 = Math.abs((double) (y1 - y2) / (x1 - x2));
+        double slopeCase2 = Math.abs((double) (y3 - y4) / (x3 - x4));
+        if (slopeCase1 == slopeCase2) {
+            return 1;
+        }
+
+        double slopeCase3 = Math.abs((double) (y1 - y3) / (x1 - x3));
+        double slopeCase4 = Math.abs((double) (y2 - y4) / (x2 - x4));
+        if (slopeCase3 == slopeCase4) {
+            return 1;
+        }
+
+        double slopeCase5 = Math.abs((double) (y1 - y4) / (x1 - x4));
+        double slopeCase6 = Math.abs((double) (y2 - y3) / (x2 - x3));
+        if (slopeCase5 == slopeCase6) {
+            return 1;
+        }
+
+        System.out.println("slopeCase1 = " + slopeCase1);
+        System.out.println("slopeCase2 = " + slopeCase2);
+        System.out.println("slopeCase3 = " + slopeCase3);
+        System.out.println("slopeCase4 = " + slopeCase4);
+        System.out.println("slopeCase5 = " + slopeCase5);
+        System.out.println("slopeCase6 = " + slopeCase6);
+
+        return answer;
     }
 
 }
