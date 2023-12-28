4972c8064b7b6f61912e02928cb4593e84c0f694
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-12-29 03:25:11 +0900

    문자열 반복해서 출력하기

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,47 +1,18 @@
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
 
-        String[] babbling1 = {
-                "aya",
-                "yee",
-                "u",
-                "maa",
-                "wyeoo"
-        };
-
-        String[] babbling2 = {
-                "ayaye",
-                "uuuma",
-                "ye",
-                "yemawoo",
-                "ayaa"
-        };
-
-        System.out.println(new Solution().solution(babbling1));
+        System.out.println(new Solution().solution());
     }
 
-    public int solution(String[] babbling) {
-        int answer = 0;
-
-        String[] accents = {
-                "aya",
-                "ye",
-                "woo",
-                "ma"
-        };
-
-        for (String b : babbling) {
-            for (String accent : accents) {
-                b = b.replace(accent, " ");
-            }
-            System.out.println(b);
-            if (b.isBlank()) {
-                answer++;
-            }
-        }
-
-        return answer;
+    public String solution() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        
+        return str.repeat(n);
     }
 
 }
