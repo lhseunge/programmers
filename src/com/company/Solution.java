4d486b299b3e353ab8b2363001c84b262a44736f
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-12-29 03:23:00 +0900

    옹알이 (1)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -2,61 +2,44 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        int[][] lines = {
-                {1, 4},
-                {9, 2},
-                {3, 8},
-                {11, 6}
+        String[] babbling1 = {
+                "aya",
+                "yee",
+                "u",
+                "maa",
+                "wyeoo"
         };
 
-        int[][] lines2 = {
-                {3, 5},
-                {4, 1},
-                {2, 4},
-                {5, 10}
+        String[] babbling2 = {
+                "ayaye",
+                "uuuma",
+                "ye",
+                "yemawoo",
+                "ayaa"
         };
 
-        System.out.println(new Solution().solution(lines));
+        System.out.println(new Solution().solution(babbling1));
     }
 
-    public int solution(int[][] lines) {
+    public int solution(String[] babbling) {
         int answer = 0;
 
-        int x1 = lines[0][0];
-        int y1 = lines[0][1];
-        int x2 = lines[1][0];
-        int y2 = lines[1][1];
-        int x3 = lines[2][0];
-        int y3 = lines[2][1];
-        int x4 = lines[3][0];
-        int y4 = lines[3][1];
+        String[] accents = {
+                "aya",
+                "ye",
+                "woo",
+                "ma"
+        };
 
-        // 선분 별 각각의 기울기 계산
-
-        double slopeCase1 = Math.abs((double) (y1 - y2) / (x1 - x2));
-        double slopeCase2 = Math.abs((double) (y3 - y4) / (x3 - x4));
-        if (slopeCase1 == slopeCase2) {
-            return 1;
+        for (String b : babbling) {
+            for (String accent : accents) {
+                b = b.replace(accent, " ");
+            }
+            System.out.println(b);
+            if (b.isBlank()) {
+                answer++;
+            }
         }
-
-        double slopeCase3 = Math.abs((double) (y1 - y3) / (x1 - x3));
-        double slopeCase4 = Math.abs((double) (y2 - y4) / (x2 - x4));
-        if (slopeCase3 == slopeCase4) {
-            return 1;
-        }
-
-        double slopeCase5 = Math.abs((double) (y1 - y4) / (x1 - x4));
-        double slopeCase6 = Math.abs((double) (y2 - y3) / (x2 - x3));
-        if (slopeCase5 == slopeCase6) {
-            return 1;
-        }
-
-        System.out.println("slopeCase1 = " + slopeCase1);
-        System.out.println("slopeCase2 = " + slopeCase2);
-        System.out.println("slopeCase3 = " + slopeCase3);
-        System.out.println("slopeCase4 = " + slopeCase4);
-        System.out.println("slopeCase5 = " + slopeCase5);
-        System.out.println("slopeCase6 = " + slopeCase6);
 
         return answer;
     }
