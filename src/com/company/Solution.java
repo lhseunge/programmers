96d24aaeb7b0f09cf75b425872d0811a5592ddb5
lhseunge <107404774+lhseunge@users.noreply.github.com>
2023-12-29 03:42:02 +0900

    n 번째 원소까지
    Arrays 클래스에 copyOfRange라는 메소드가 이미 있다. 
    
    문제 해결을 위해 리스트를 사용하기보다 배열을 컨트롤하기 위해 관련 메소드를 연상하거나 찾아보는 것이 성능에 도움이 될듯.

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution());
+       int[] num_list = {
+                5, 2, 1, 7, 5
+        };
+
+        System.out.println(Arrays.toString(new Solution().solution(num_list, 3)));
     }
 
-    public String solution() {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = sc.nextInt();
-        
-        return str.repeat(n);
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            answer.add(num_list[i]);
+        }
+
+        return answer.stream().mapToInt(x->x).toArray();
     }
 
 }
